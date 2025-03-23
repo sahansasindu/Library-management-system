0e35ef3d161c75951d453c0e4a2e5fb8c6498493
sahansasindu <sahan_2020742@fot.ruh.ac.lk>
2025-03-23 10:55:24 +0530

    add

--- src/main/java/com/example/Library/Management/System/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.example.Library.Management.System.service;
-
-import org.springframework.stereotype.Service;
-
-
-public interface UserService {
-}
--- src/main/java/com/example/Library/Management/System/service/impl/UserServiceImpl.java
+++
@@ -1,7 +0,0 @@
-package com.example.Library.Management.System.service.impl;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class UserServiceImpl {
-}
