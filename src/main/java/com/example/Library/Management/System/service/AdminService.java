38ce99f820c79480ae9423149b238d4a62f560af
sahansasindu <sahan_2020742@fot.ruh.ac.lk>
2025-03-25 01:09:56 +0530

    add

--- src/main/java/com/example/Library/Management/System/service/AdminService.java
+++ src/main/java/com/example/Library/Management/System/service/AdminService.java
@@ -2,7 +2,11 @@ package com.example.Library.Management.System.service;
 
 import com.example.Library.Management.System.dto.MemberDto;
 
+import java.util.List;
+
 public interface AdminService {
 
     public void addNewMember(MemberDto memberDTO);
+
+    public List<MemberDto>getAllMembers();
 }
