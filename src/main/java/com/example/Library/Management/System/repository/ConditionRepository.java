e6d4d84f2a0e4a524a5ee922545c0ec7eda82af4
Sahan Sasindu <119693671+sahansasindu@users.noreply.github.com>
2026-05-09 12:30:21 +0530

    update code

--- src/main/java/com/example/Library/Management/System/repository/ConditionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Library.Management.System.repository;
-
-import com.example.Library.Management.System.entity.Condition;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ConditionRepository extends JpaRepository<Condition,Integer> {
-}
---
+++ src/main/java/com/example/Library/Management/System/service/impl/RAGService.java
@@ -0,0 +1,4 @@
+package com.example.Library.Management.System.service.impl;
+
+public class RAGService {
+}
